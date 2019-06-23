ba386f59b7b97dca349eb24e9df01ee20beae0dc
jacoloves <5511068t@gmail.comn>
2019-06-23 18:45:28 +0900

    Python_Tutorial

---
+++ araisandayo_Python/chap1/pra1.py
@@ -0,0 +1,4 @@
+name = input('What is your name? ')
+color = input('What is favorite color? ')
+
+print(name + " likes " + color)
---
+++ araisandayo_Python/chap1/pra2.py
@@ -0,0 +1,3 @@
+weight = input("How many you weight?(in pounds) ")
+kg_wegiht = float(weight) * 0.45
+print(kg_wegiht)
